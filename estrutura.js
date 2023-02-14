e82759622639a0ce096333d53d52f0495da9b68a
bruno <brunoribeiro58929@gmail.com>
2023-02-14 17:07:51 -0300

    exercicio estrutura de dados

--- estrutura.js
+++ estrutura.js
@@ -1,1 +1,45 @@
-const nuber=10
+const boolan= true
+const string ="bruno"
+const nuber= 34
+const floalt=21.3
+const sem_resultado =undefined
+const nula= null
+const array= []//lista
+const objeto= {}
+
+
+
+const celula= [
+   {
+    numero_da_sala:1,
+    menbro: ["ana","vitor","maria"],
+    maria_demitida: true,
+    vitor_recebe_uma_coca:true,
+    {lider: "pedro",
+     setor: 3}
+
+
+   }
+   {
+    numero_da_sala:2,
+    menbro: ["joao","henrique","jose"],
+    joao_demitida: true,
+    jose_recebe_2.300:true,
+    {lider: "otavio",
+     setor: 3}
+
+   }
+   {
+    numero_da_sala:3,
+    menbro: ["carol","vitoria","mariana"],
+    mariana_demitida: true,
+    vitoria_recebe_2.300:true,
+    {lider: "pedro",
+     setor: 3}
+   }
+
+
+
+
+
+]
